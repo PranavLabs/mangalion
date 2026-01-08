bad46edcb8f6e101c23bbcc0f94a5f745a4eae27
Pranav Gandha <pranavgandha@hotmail.com>
2026-01-09 02:38:54 +0530

    Add transpilePackages for compatibility
    Added transpilePackages to ensure compatibility with specific packages.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // This tells Next.js to compile these packages so they don't break the build
+  transpilePackages: ['@consumet/extensions', 'undici', 'cheerio'],
+  
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
